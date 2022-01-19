b74364a94142815c303a2ebec8ee61c410e46df5
Timur Khakkiev <khakkievtimur@yandex.ru>
2022-01-19 07:56:32 +0300

    refactor startApolloServer

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import typeDefs from './schema.js'
 import { resolvers } from './resolvers.js'
 import { TrackAPI } from './datasources/track-api.js'
 
-const trackAPI = new TrackAPI()
+async function startApolloServer(typeDefs, server) {
+  const trackAPI = new TrackAPI()
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  // returns an object with trackApi prop which was instatiated with TrackApi class
-  dataSources: () => ({ trackAPI }),
-})
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // returns an object with trackApi prop which was instatiated with TrackApi class
+    dataSources: () => ({ trackAPI }),
+  })
 
-server.listen({ port: process.env.PORT || 4000 }).then(() => {
+  const { url } = await server.listen({
+    port: process.env.PORT || 4000,
+  })
+
   console.log(`
     🚀  Server is running!
     🔉  Listening on port 4000
     📭  Query at https://studio.apollographql.com/dev
   `)
-})
+}
+
+startApolloServer(typeDefs, server)
